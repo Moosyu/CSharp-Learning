72a26899103789f0e6203708a6dcbc5245296ab7
Moosyu <moosemoosetwutwu@gmail.com>
2022-01-16 13:57:48 +1300

    removed the timer functions, ill add them back in later

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,34 +1,22 @@
 ﻿using System;
-using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 public class Sus
 {
-    //Thank god for google, would have never figured this out
-    private static System.Timers.Timer aTimer;
 
     static void Main()
     {
-        SetTimer();
 
-        Console.WriteLine("\nPress the Enter key to continue the application...\n");
-
-        //hh is hours, mm is minutes and ss is seconds
-        Console.WriteLine("The application started at {0:HH:mm:ss}", DateTime.Now);
         
-        Console.ReadLine();
-
-        static void SetTimer()
-        {
-            aTimer = new System.Timers.Timer(2000);
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
         Random rnd = new Random();
 
 
         string[] names = {
-            "joe", "chris", "rx", "rby", "friend", "sus", "test"
+            "joe", "chris", "george", "phil", "friend", "sus", "test"
         };
 
 
@@ -42,7 +30,6 @@ public class Sus
         //Legit the most hamfisted solution I have ever used also I have no idea why I to pad right 6 times for one space please explain 
         Console.WriteLine("Hello".PadRight(6) + names[index]);
 
-        aTimer.Stop();
 
         Console.WriteLine("The application ended at {0:HH:mm:ss}", DateTime.Now);
 
